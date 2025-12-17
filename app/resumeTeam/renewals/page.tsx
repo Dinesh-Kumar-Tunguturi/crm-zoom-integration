a48112bb49b43ef23a1dc0251fd221bb7bbbdbda
Tech-Applywizz-git-account <techteam@Applywizz.com>
2025-12-17 13:18:45 +0530

    updated the auto_generating_portfolio_url renwals is changed

--- app/resumeTeam/renewals/page.tsx
+++ app/resumeTeam/renewals/page.tsx
@@ -1300,21 +1300,19 @@ export default function JobBoardPage() {
                     </a>
                   ) : row.leads?.name ? (
                     <a
-                      href={`https://applywizz-${(row.leads?.name ||
-                        "")
+                      href={`https://applywizz-${(row.leads?.name || "")
                         .toLowerCase()
-                        .replace(/[^a-z0-9]/g, "")}.vercel.app/`}
+                        .replace(/[^a-z0-9]/g, "")}-${(row.lead_id || "").replace(/\D/g, "")}.vercel.app/`}
                       target="_blank"
                       rel="noreferrer"
                       className="text-blue-600 underline block truncate"
-                      title={`https://applywizz-${(row.leads?.name ||
-                        "")
+                      title={`https://applywizz-${(row.leads?.name || "")
                         .toLowerCase()
-                        .replace(/[^a-z0-9]/g, "")}.vercel.app/`}
+                        .replace(/[^a-z0-9]/g, "")}-${(row.lead_id || "").replace(/\D/g, "")}.vercel.app/`}
                     >
                       {`https://applywizz-${(row.leads?.name || "")
                         .toLowerCase()
-                        .replace(/[^a-z0-9]/g, "")}.vercel.app/`}
+                        .replace(/[^a-z0-9]/g, "")}-${(row.lead_id || "").replace(/\D/g, "")}.vercel.app/`}
                     </a>
                   ) : (
                     <span className="text-gray-400 text-sm">—</span>
